309ef04bb8985524aa9301d05bbebb317e2289dc
jclopeza <josecarlos.lopezayala@gmail.com>
2018-08-27 22:40:59 +0200

    New script for finding out the new version

--- settingNewDarVersion.py
+++ settingNewDarVersion.py
@@ -1,5 +1,6 @@
 lastVersion="${lastVersion}"
 array=lastVersion.split("-B")
 newBuild=int(array[1])+1
-releaseVariables['versionNumber']=array[0]+"-B"+str(newBuild)
+darVersion=array[0]+"-B"+str(newBuild)
+releaseVariables['versionNumber']=darVersion
 
