b322ee6f1137eaa2c2f62bb8e85d516db512948d
José Carlos López Ayala <josecarlos.lopezayala@gmail.com>
2018-09-18 15:31:28 +0200

    updated

--- settingNewDarVersion.py
+++ settingNewDarVersion.py
@@ -1,6 +1,6 @@
-lastVersion="${lastVersion}"
-array=lastVersion.split("-B")
+lastVersion="${last-version}"
+array=lastVersion.split("/")
+array=array[-1].split("-B")
 newBuild=int(array[1])+1
 darVersion=array[0]+"-B"+str(newBuild)
-releaseVariables['versionNumber']=darVersion
-
+releaseVariables['next-version']=darVersion
