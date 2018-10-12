33bd9d18f89ec6ad4e479b8017e9fe30b0c51e3a
José Carlos López Ayala <josecarlos.lopezayala@gmail.com>
2018-10-12 12:44:58 +0200

    new message

--- settingNewDarVersion.py
+++ settingNewDarVersion.py
@@ -2,5 +2,5 @@ lastVersion="${last-version}"
 array=lastVersion.split("/")
 array=array[-1].split("-B")
 newBuild=int(array[1])+1
-darVersion=array[0]+"-B"+str(newBuild)
+darVersion="${component-version}"+"-B"+str(newBuild)
 releaseVariables['next-version']=darVersion
