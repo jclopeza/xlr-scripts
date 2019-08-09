b7d2dd8f1e20e403c4f51367a5b0b7caa0f46ce0
José Carlos <josecarlos.lopezayala@gmail.com>
2019-08-09 18:16:15 +0200

    informe html para terraform

---
+++ generateTerraformHtmlReport.py
@@ -0,0 +1,67 @@
+from jinja2 import Template
+import datetime
+import sys
+
+# Recogemos variables
+environment = sys.argv[1]
+projectName = sys.argv[2]
+awsRegion = sys.argv[3]
+instanceType = sys.argv[4]
+
+t = Template("""
+<html>
+  <head>
+    <title>
+      Grafico generado para el proyecto {{ projectName }} y entorno {{ environment }}
+    </title>
+  </head>
+  <body>
+    <p>
+      <center><h2>Infraestructura desplegada para el proyecto <u>{{ projectName }}</u> y entorno <u>{{ environment }}</u></h2></center>
+    </p>
+    <p>
+      <table width="100%" border=1>
+        <tr>
+          <td width="20%" align=center>
+            <table>
+              <tr>
+                <td><b>Region AWS</b></td><td width="15%"></td><td>{{ awsRegion }}</td>
+              </tr>
+              <tr>
+                <td><b>Proyecto</b></td><td width="15%"></td><td>{{ projectName }}</td>
+              </tr>
+              <tr>
+                <td><b>Entorno</b></td><td width="15%"></td><td>{{ environment }}</td>
+              </tr>
+              <tr>
+                <td><b>Instancias EC2</b></td><td width="15%"></td><td>{{ instanceType }}</td>
+              </tr>
+              <tr>
+                <td><b>Fecha</b></td><td width="15%"></td><td>{{ date }}</td>
+              </tr>
+              <tr>
+                <td><b>Hora</b></td><td width="15%"></td><td>{{ hour }}</td>
+              </tr>
+            </table>
+          </td>
+          <td width="80%" align=center>
+            <img src="graph.svg" width="80%">
+          </td>
+        </tr>
+      </table>
+    </p>
+  </body>
+</html>
+""")
+x = datetime.datetime.now()
+htmlGenerated = t.render(
+    projectName=projectName,
+    environment=environment,
+    awsRegion=awsRegion,
+    instanceType=instanceType,
+    date=x.strftime("%d/%m/%Y"),
+    hour=x.strftime("%H:%M:%S")
+    )
+f = open("/var/www/html/{0}-{1}/index.html".format(projectName, environment), "w")
+f.write(htmlGenerated)
+f.close()
